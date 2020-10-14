d2af6ba2eb43b1b775eab064df2280e47724853a
Ivan Noskov <vaneknoskov@mail.ru>
2020-10-14 22:29:29 +0300

    Initial commit.

--- src/module01/Lesson3DataTypes.java
+++ src/module01/Lesson3DataTypes.java
@@ -2,6 +2,9 @@ package module01;
 
 public class Lesson3DataTypes {
     public static void main(String[] args) {
-
+        byte a = 127;
+        byte b = (byte)255;
+        byte c = (byte) (a - b);
+        System.out.println("Result: " + c);
     }
 }
