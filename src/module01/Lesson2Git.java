8e85fd6dec7726188ccdcf5b6b5f6b5a470deaae
Ivan Noskov <vaneknoskov@mail.ru>
2020-10-14 22:23:53 +0300

    Initial commit.

---
+++ src/module01/Lesson2Git.java
@@ -0,0 +1,7 @@
+package module01;
+
+public class Lesson2Git {
+    public static void main(String[] args) {
+        System.out.println("I have installed GIT");
+    }
+}
---
+++ src/module01/Lesson3DataTypes.java
@@ -0,0 +1,7 @@
+package module01;
+
+public class Lesson3DataTypes {
+    public static void main(String[] args) {
+
+    }
+}
