43d9d63fc3f00602ff19e44ada2369994f2f4d89
Ivan Noskov <vaneknoskov@mail.ru>
2020-10-14 22:32:05 +0300

    Initial commit.

--- src/module01/Lesson3DataTypes.java
+++ src/module01/Lesson3DataTypes.java
@@ -6,5 +6,7 @@ public class Lesson3DataTypes {
         byte b = (byte)255;
         byte c = (byte) (a - b);
         System.out.println("Result: " + c);
+
+        // Байтовый тип данных содержит от -128 до 127 значений и занимает 8 бит памяти.
     }
 }
