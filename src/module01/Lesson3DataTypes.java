04a103ad5eab0857d6fe4da0e98782c07e29d6ae
Ivan Noskov <vaneknoskov@mail.ru>
2020-10-15 23:10:25 +0300

    Initial commit.

--- src/module01/Lesson3DataTypes.java
+++ src/module01/Lesson3DataTypes.java
@@ -1,5 +1,5 @@
 package module01;
-
+                            /////////// TASK 31 ////////////
 public class Lesson3DataTypes {
     public static void main(String[] args) {
         byte a = 127;
@@ -9,4 +9,35 @@ public class Lesson3DataTypes {
 
         // Байтовый тип данных содержит от -128 до 127 значений и занимает 8 бит памяти.
     }
+
+    void programTask32(){
+        short a = -32768;
+        short b = 32767;
+        short c = (short)(a + b);
+        System.out.println("Result: " + c);
+
+        // Тип данных short содержит от -32768 до 32767 значений и занимает 16 бит памяти
+    }
+
+    void programTask33(){
+        int x = 1200;
+        System.out.println(x);
+        System.out.println(Math.sqrt(x));
+        System.out.println(Math.cbrt(x));
+
+        // Тип данных int содержит от -2 147 483 648 до 2 147 483 647 и занимает 32 бита памяти
+
+    }
+
+    void programTask34(){
+        long l = Long.MAX_VALUE;
+
+        // Возможно использовать для замера времени в секундах запущеных у устройства с момента включения
+    }
+
 }
+
+
+
+
+
