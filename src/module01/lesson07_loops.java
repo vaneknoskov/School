02d7a37d3e4e3d36c87bfe0379d15f46103be86b
Ivan Noskov <vaneknoskov@mail.ru>
2020-10-26 23:42:02 +0300

    Initial commit.

--- src/module01/lesson07_loops.java
+++ src/module01/lesson07_loops.java
@@ -1,4 +1,34 @@
 package module01;
 
+import java.util.Scanner;
+
 public class lesson07_loops {
-}
+    public static void main(String[] args) {
+        System.out.println("Task 1");
+        for(int i = 10; i <= 20; i++){
+            System.out.println("Квадрат числа от " + i + " = " + (i * i));
+        }
+
+        System.out.println("Task 2");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите N: ");
+        int N = scanner.nextInt();
+        int i = 1;
+        while(i * i <= N){
+            System.out.println("Квадрат числа от " + i + " = " + (i * i));
+            i++;
+        }
+
+        System.out.println("Task 3");
+        int [] array = {10, 20, 15, 17, 24, 35};
+        int mul = 1;
+        for (int x : array) {
+            mul *= x;
+        }
+        System.out.println(mul);
+
+        System.out.println("Task 4");
+
+
+        }
+    }
