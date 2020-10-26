2944c5bf1f39cd12d1af377c04df566e1e3f0b5b
Ivan Noskov <vaneknoskov@mail.ru>
2020-10-26 20:37:24 +0300

    Initial commit.

--- src/module01/Lesson5Conditions.java
+++ src/module01/Lesson5Conditions.java
@@ -1,6 +1,8 @@
 package module01;
 
 
+import java.util.Scanner;
+
 public class Lesson5Conditions {
     public static void main(String[] args) {
         System.out.println("Task 1." + "\n" + "Ничего не будет выведено на консоль." + "\n");
@@ -58,6 +60,60 @@ public class Lesson5Conditions {
         String message = "";
         String result = num > 10 ? "Number is greater than 10" : "Number is less than or equal to 10";
         System.out.println(result);
+
+        System.out.println("Task 6");
+        System.out.println("Введите любое число: ");
+        Scanner scanner = new Scanner(System.in);
+        int month = scanner.nextInt();
+        switch (month){
+            case 1: case 2: case 12:
+                System.out.println("Зима");
+                break;
+            case 3: case 4: case 5:
+                System.out.println("Весна");
+                break;
+            case 6: case 7: case 8:
+                System.out.println("Лето");
+                break;
+            case 9: case 10: case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Введите чило от 1 до 12");
+        }
+
+        System.out.println("Task 7");
+        System.out.println("Enter the day of the week.");
+        Scanner scanner1 = new Scanner(System.in);
+        String day = scanner1.next().toLowerCase(); // it can be any day
+
+        switch(day) {
+            case "monday":
+                System.out.println("This is the first day of the week");
+                break;
+            case "tuesday":
+                System.out.println("This is the second day of the week");
+                break;
+            case "wednesday":
+                System.out.println("This is the third day of the week");
+                break;
+            case "thursday":
+                System.out.println("This is the fourth day of the week");
+                break;
+            case "friday":
+                System.out.println("This is the fifth day of the week");
+                break;
+            case "saturday":
+                System.out.println("This is the sixth day of the week");
+                break;
+            case "sunday":
+                System.out.println("This is the seventh day of the week");
+                break;
+            default:
+                System.out.println("Such day of the week doesn't exist.");
+        }
+
+        System.out.println("The end. ^^");
     }
 }
 
