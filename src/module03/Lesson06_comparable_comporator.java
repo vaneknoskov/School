68a879c3fb748a8e2ecb420d32f8106b55296633
Ivan Noskov <vaneknoskov@mail.ru>
2020-10-29 23:00:34 +0300

    Initial commit.

--- src/module03/Lesson06_comparable_comporator.java
+++ src/module03/Lesson06_comparable_comporator.java
@@ -8,10 +8,10 @@ public class Lesson06_comparable_comporator {
 
 
     public static void main(String[] args) {
-        Car car = new Car("X6", 260);
+        Car car = new Car("Audi", 260);
         Car car1 = new Car("YAZ", 60);
-        Car car2 = new Car("Lancer", 200);
-        Car car3 = new Car("Lada 2110", 160);
+        Car car2 = new Car("Mercedes", 230);
+        Car car3 = new Car("Lada", 160);
 
         ArrayList<Car> auto = new ArrayList<>();
         auto.add(car);
@@ -23,7 +23,11 @@ public class Lesson06_comparable_comporator {
 
         System.out.println(auto);
 
+        ComparatorAuto comparatorAuto = new ComparatorAuto();
 
+        Collections.sort(auto, comparatorAuto);
+
+        System.out.println(auto);
     }
 }
 
@@ -44,7 +48,7 @@ class Car implements Comparable<Car>{
     }
     @Override
     public int compareTo(Car o) {
-        return Integer.compare(speed, o.speed);
+        return Integer.compare(speed, o.speed); // сортировка скорости по возрастанию
 
     }
 
@@ -54,6 +58,6 @@ class ComparatorAuto implements Comparator <Car> {
 
     @Override
     public int compare(Car o1, Car o2) {
-        return o1.model.compareTo(o2.model);
+        return o2.model.compareTo(o1.model); // сортировка по моделям по убиванию(в алф. пор.)
     }
 }
