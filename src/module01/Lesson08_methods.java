2112d3aeb93d12a5f930bd3c9c22d986e33e9fa6
Ivan Noskov <vaneknoskov@mail.ru>
2020-11-02 23:04:59 +0300

    Initial commit.

--- src/module01/Lesson08_methods.java
+++ src/module01/Lesson08_methods.java
@@ -2,6 +2,25 @@ package module01;
 
 public class Lesson08_methods {
     public static void main(String[] args) {
+        evenAndOdd();
+        fromOneToX(20);
+    }
 
+    static void evenAndOdd() {
+        for (int i = 0; i <= 20; i++) {
+            if (i % 2 == 1) {
+                System.out.print("Нечетное - " + i + " ");
+            } else {
+                System.out.println("Четное - " + i);
+            }
+        }
+    }
+
+    static void fromOneToX(int x){
+        int sum = 0;
+        for(int i = 1; i<= x; i++){
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }
