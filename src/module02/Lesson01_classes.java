b7b5810f2c6449bed651c8c2d86b9b7c9ed405ee
Ivan Noskov <vaneknoskov@mail.ru>
2020-11-23 19:50:07 +0300

    Initial commit.

--- src/module02/Lesson01_classes.java
+++ src/module02/Lesson01_classes.java
@@ -7,6 +7,8 @@ public class Lesson01_classes {
     public static void main(String[] args) {
         Person person = new Person("Ivan", 27);
         person.meditation();
+        Cat myCat = new Cat("Myrka", 2, Cat.Breed.SIAMESE, person);
+        System.out.println(myCat);
 
     }
 }
@@ -39,11 +41,30 @@ class Person{
 class Cat{
     String name;
     int age;
+    Breed breed;
+    Person owner;
+
+    public Cat(String name, int age, Breed breed, Person owner) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+        this.owner = owner;
+    }
 
     enum Breed {
         SIAMESE,
         NEBELUNG,
         BIRMAN;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", breed=" + breed +
+                ", owner=" + owner +
+                '}';
     }
 }
 
